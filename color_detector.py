35740547cd1708f935eed64e573a766ce6106bf1
sz374 <sz374@cornell.edu>
2021-04-29 18:01:02 -0400

    changed color detection to ciede2000

--- color_detector.py
+++ color_detector.py
@@ -6,6 +6,9 @@ from constants import (
     SATURATION_INCREASE_VALUE,
     CUBE_SIDE,
     COLORS,
+    CLOSE_THRESHOLD,
+    CIEDE2000,
+    BGR2LAB,
 )
 
 
@@ -18,12 +21,22 @@ def find_color(input_color):
     return {i: key for i, key in enumerate(COLORS)}[min_idx]
 
 
+def find_color_ciede2000(lab):
+    dists = []
+    for color in COLORS:
+        r, g, b = COLORS[color]
+        lab2 = BGR2LAB((b, g, r))
+        dists.append((color, CIEDE2000(lab, lab2)))
+    name, _ = min(dists, key=lambda item: item[1])
+    return name
+
+
 def preprocess_img(img):
     # convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # blur to remove noise, kernel size set to large because
     # don't care about details, we just want to extract the color.
-    blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
     # perform edge detection with canny edge detector
     canny = cv2.Canny(blurred, 20, 40)
     # make lines thicker by dilating
@@ -32,29 +45,60 @@ def preprocess_img(img):
     return dilated
 
 
+def is_close_to(cnt1, cnt2):
+    corner1 = (
+        np.sqrt(
+            (cnt1[0][0][0] - cnt2[0][0][0]) ** 2 + (cnt1[0][0][1] - cnt2[0][0][1]) ** 2
+        )
+        <= CLOSE_THRESHOLD
+    )
+    corner2 = (
+        np.sqrt(
+            (cnt1[2][0][0] - cnt2[2][0][0]) ** 2 + (cnt1[2][0][1] - cnt2[2][0][1]) ** 2
+        )
+        <= CLOSE_THRESHOLD
+    )
+    return corner1 and corner2
+
+
 def squares_from_contours(dilated):
     contours, _ = cv2.findContours(
         dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    approxs = []
+    approxs = [
+        cv2.approxPolyDP(contour, 0.1 * cv2.arcLength(contour, True), closed=True)
+        for contour in contours
+    ]
 
-    for contour in contours:
-        epsilon = 0.1 * cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, epsilon, closed=True)
-        area = cv2.contourArea(approx)
-        side_length = np.sqrt(area)
-        # render if satisfy the following conditions:
-        # 1. 4 corners
-        # 2. [side_length], or square root of [area] is
-        # within the defined threshold (requires some tuning)
-        if (
-            len(approx) == 4
-            and cv2.isContourConvex(approx)
-            and side_length > MIN_SIDE_LENGTH_THRESHOLD
-            and side_length < MAX_SIDE_LENGTH_THRESHOLD
-        ):
-            approxs.append(approx)
+    # filter out polygons that do not have 4 sides
+    # filter out polygons that do not match the side length thresholds
+    approxs = list(
+        filter(
+            lambda approx: len(approx) == 4
+            and np.sqrt(cv2.contourArea(approx)) > MIN_SIDE_LENGTH_THRESHOLD
+            and np.sqrt(cv2.contourArea(approx)) < MAX_SIDE_LENGTH_THRESHOLD,
+            approxs,
+        )
+    )
+
+    # results = []
+
+    # # filtering overlapping contours by checking two opposite corners
+    # flagged_approxs = set()
+
+    # for i in range(len(approxs)):
+    #     a1 = approxs[i]
+    #     if i in flagged_approxs:
+    #         continue
+    #     for j in range(len(approxs)):
+    #         if i == j:
+    #             continue
+    #         a2 = approxs[j]
+    #         if is_close_to(a1, a2):
+    #             flagged_approxs.add(j)
+    #             break
+    #     results.append(a1)
 
     return approxs
 
@@ -89,7 +133,7 @@ def compute_color_locs(img, name):
 
     contours = squares_from_contours(dilated)
 
-    img = increase_saturation(img)
+    # img = increase_saturation(img)
 
     sorted_contours = sort_contours_by_pos(contours)
 
@@ -98,7 +142,7 @@ def compute_color_locs(img, name):
     # extract color
     for i in range(len(sorted_contours)):
         approx = sorted_contours[i]
-        cv2.drawContours(img, [approx], -1, (0, 255, 255), 5)
+        cv2.drawContours(img, [approx], -1, (0, 255, 0), 10)
         mid_x = 0
         mid_y = 0
         for point in approx:
@@ -107,11 +151,14 @@ def compute_color_locs(img, name):
         mid_x /= 4
         mid_y /= 4
         b, g, r = img[int(mid_y)][int(mid_x)]
-        r, g, b = COLORS[find_color((r, g, b))]
+
+        lab = BGR2LAB((b, g, r))
+
+        r, g, b = COLORS[find_color_ciede2000(lab)]
         if i % 3 == 0:
-            color_locs.append([find_color((r, g, b))])
+            color_locs.append([find_color_ciede2000(lab)])
         else:
-            color_locs[-1].append(find_color((r, g, b)))
+            color_locs[-1].append(find_color_ciede2000(lab))
         cv2.fillPoly(img, [approx], (b, g, r))
 
     cv2.imwrite("output/%s.png" % name, img)
@@ -119,5 +166,5 @@ def compute_color_locs(img, name):
 
 
 if __name__ == "__main__":
-    img = cv2.imread("data/1.jpeg")
-    print(compute_color_locs(img, "img1"))
+    img = cv2.imread("data/4.jpeg")
+    print(compute_color_locs(img, "img4"))
